2449931dfefc5be5c358e963f33aa8b84d22371d
Kyle Bibler <kyle.bibler08@gmail.com>
2015-12-21 01:19:00 -0500

    Some debugging

--- routes/lobbies.js
+++ routes/lobbies.js
@@ -8,25 +8,31 @@ var getLobbyWithPermissionCheck = function(lobbyNum, callback) {
 	var lobbyData = {
 		name: "Cool Lobby",
 		id: 1,
-		usersPresent: ["Kyle", "John", "Cody"],
+		users: ["Kyle", "John", "Cody"],
 		type: "private",
 		showAnswersToOthers: false,
 		isAnonymous: false,
 		allowAnonymous: false,
 		responses: [{text: "Something something", user: "Kyle"}]
-	}
-	return callback(lobbyData)
-}
+	};
+	return callback(lobbyData);
+};
 
 var getLobbyList = function(filters, callback) {
 	//DB query to get list of lobbies
 	//Fake for now
 	var lobbies = [
-		{name: "Cool Lobby", id: 1, isPrivate: true, usersPresent: 3},
-		{name: "Cool Lobby 2", id: 2, isPrivate: true, usersPresent: 3},
+		{name: "Cool Lobby", id: 1, isPrivate: true, usersPresent: 3, dateCreated: "12/21/15 23:54"},
+		{name: "Cool Lobby 2", id: 2, isPrivate: true, usersPresent: 3, dateCreated: "12/21/15 23:57"},
 	];
 	return callback(lobbies);
-}
+};
+
+var createLobby = function(lobbyData, callback) {
+	//Do some db magic
+	var errors = [];
+	callback(errors);
+};
 
 router.get('/:lobby?', function(req, res, next) {
 	if (req.params.lobby) {
@@ -34,7 +40,7 @@ router.get('/:lobby?', function(req, res, next) {
 		//Verify user has access to this lobby		
 		var lobbyNum = req.params.lobby;
 		getLobbyWithPermissionCheck(lobbyNum, function(lobbyData) {
-			res.render('lobby', { lobbyData: lobbyData);
+			res.render('lobby', { lobbyData: lobbyData });
 		});		
 	} else {
 		//Get list of lobbies
